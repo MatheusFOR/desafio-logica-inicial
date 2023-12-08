93eea0f70d45f72c6dee25d41bfb419f9b2bacdf
MatheusFOR <matheusfelipe31@gmail.com>
2023-12-08 16:33:47 -0300

    desafio

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Olá Mundo!")
+let xp = 80000;
+
+switch (true) {
+    case xp <= 1000:
+        console.log("Ferro");
+        break;
+    
+    case xp >= 1001 && xp <= 2000:
+        console.log("Bronze");
+        break;
+    case xp >= 2001 && xp <= 5000:
+        console.log("Prata");
+        break;
+    case xp >= 6001 && xp <= 7000:
+        console.log("Ouro");
+        break;
+    case xp >= 7001 && xp <= 8000:
+        console.log("Platina");
+        break;
+    case xp >= 8001 && xp <= 9000:
+        console.log("Ascendente");
+        break;
+    case xp >= 9001 && xp <= 10000:
+        console.log("Imortal");
+        break;
+    case xp >= 10001:
+        console.log("Radiante");
+        break;
+}
